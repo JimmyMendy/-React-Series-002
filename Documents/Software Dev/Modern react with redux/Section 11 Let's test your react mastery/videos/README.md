4096608b784ee465566c1c38a9dce68fe1493979
Jimmy Mendy <jimmy.mendy@gmail.com>
2020-11-21 15:17:09 +0000

    first commit

---
+++ Documents/Software Dev/Modern react with redux/Section 11 Let's test your react mastery/videos/README.md
@@ -0,0 +1,3 @@
+ReactSeries 003
+
+REACT video Search, this web app is a simple image research app, here the goal was to learn how to handle user input with forms and event while making api requests. here we used the Youtube API.
---
+++ Documents/Software Dev/Modern react with redux/Section 11 Let's test your react mastery/videos/src/components/SearchBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class SearchBar extends React.Component  {
+  state = { term: '' };
+
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = e => {
+    e.preventDefault();
+
+    this.props.onFormSubmit(this.state.term);
+  };
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label htmlFor="">Video Search</label>
+            <input 
+              type="text" 
+              value={this.state.term}
+              onChange={this.onInputChange}
+              placeholder="Enter a value"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
